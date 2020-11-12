ceb2d00851925a6f1765616a75143f32bb0beada
Tom <tvandort1@gmail.com>
2020-11-11 21:15:24 -0500

    Extract cg component.

---
+++ src/components/CrystallineGiantView.tsx
@@ -0,0 +1,54 @@
+import {
+  Ability,
+  CrystallineGiantInitializer,
+  CrystallineGiantReducer,
+  CrystallineGiant
+} from '@app/logic/crystallineGiant';
+import { useReducer } from 'react';
+
+export const CrystallineGiantView = ({
+  initialAbility
+}: {
+  initialAbility: Ability;
+}) => {
+  const [state, dispatch] = useReducer(
+    CrystallineGiantReducer,
+    CrystallineGiantInitializer(initialAbility)
+  );
+  const card = new CrystallineGiant(state, dispatch);
+
+  return (
+    <div>
+      Your Crystalline Giant:&nbsp;
+      <button
+        onClick={() => {
+          card.gainAbility();
+        }}
+        disabled={!card.CanGainAbility}
+        title={
+          card.CanGainAbility
+            ? 'Roll to gain another ability.'
+            : 'Cannot roll; all abilities gained.'
+        }
+      >
+        Roll
+      </button>
+      <ul>
+        {card.Abilities.map((ability) => (
+          <li key={ability}>{ability}</li>
+        ))}
+      </ul>
+      <br />
+      <br />
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          card.reset();
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,60 +1,15 @@
-import {
-  Ability,
-  AllAbilities,
-  CrystallineGiantInitializer,
-  CrystallineGiantReducer,
-  CrystallineGiant,
-  Pick
-} from '@app/logic/crystallineGiant';
+import { Ability, AllAbilities, Pick } from '@app/logic/crystallineGiant';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
-import { useEffect, useReducer, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { CrystallineGiantView } from '../components/CrystallineGiantView';
 
 interface HomeProps {
   initialAbility: Ability;
 }
 
 export default function Home({ initialAbility }: HomeProps) {
-  const [state, dispatch] = useReducer(
-    CrystallineGiantReducer,
-    CrystallineGiantInitializer(initialAbility)
-  );
-  const card = new CrystallineGiant(state, dispatch);
-
-  return (
-    <div>
-      Your Crystalline Giant:&nbsp;
-      <button
-        onClick={() => {
-          card.gainAbility();
-        }}
-        disabled={!card.CanGainAbility}
-        title={
-          card.CanGainAbility
-            ? 'Roll to gain another ability.'
-            : 'Cannot roll; all abilities gained.'
-        }
-      >
-        Roll
-      </button>
-      <ul>
-        {card.Abilities.map((ability) => (
-          <li key={ability}>{ability}</li>
-        ))}
-      </ul>
-      <br />
-      <br />
-      <br />
-      <br />
-      <button
-        onClick={() => {
-          card.reset();
-        }}
-      >
-        Reset
-      </button>
-    </div>
-  );
+  return <CrystallineGiantView initialAbility={initialAbility} />;
 }
 
 export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
